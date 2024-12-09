e17f712e33758b4745418f256a5e3538ca92280e
NithishAK31 <nithish2006ak@gmail.com>
2024-12-09 22:26:26 +0530

    Till weather of delhi in center

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'df07a4cf71mshca6db58d9a4a559p1d5b8ejsn89e0bbf3e69c',
+		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
