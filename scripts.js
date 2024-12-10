c1ecdcfd864769d65ed653b4706adadeb870b866
NithishAK31 <nithish2006ak@gmail.com>
2024-12-10 13:43:34 +0530

    js till example output look

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi';
 const options = {
 	method: 'GET',
 	headers: {
@@ -9,8 +9,20 @@ const options = {
 
 try {
 	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
+	const response = await response.text();
+	// console.log(result);
+	console.log(response);
+	cloud_pct.innerHTML = response.cloud_pct
+	temp.innerHTML = response.temp
+	feels_like.innerHTML = response.feels_like
+	humidity.innerHTML = response.humidity
+	min_temp.innerHTML = response.min_temp
+	max_temp.innerHTML = response.max_temp
+	wind_speed.innerHTML = response.wind_speed
+	wind_degrees.innerHTML = response.wind_degrees
+	sunrise.innerHTML = response.sunrise
+	sunset.innerHTML = response.sunset
+
 } catch (error) {
 	console.error(error);
 }
